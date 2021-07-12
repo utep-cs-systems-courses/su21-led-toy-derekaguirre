799632fe1cd05d48586c75a417916499a7dc4ee3
derekaguirre <derekaguirre@me.com>
2021-07-12 02:14:40 -0600

    First 3 buttons do something unique and the last button turns off the speaker/leds

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -1,23 +1,62 @@
 #include <msp430.h>
 #include "stateMachines.h"
-static char time = 0;
+#include "switches.h"
+
 static char mode = 0;
 static char blink_count = 0;
+static char time = 0;
+
+char switch_state = 0;
 
 void
 __interrupt_vec(WDT_VECTOR) WDT(){ /* 250 interrupts/sec */
-  if(time == 249){ /*Iterates mode every second*/
-    time = 0;
-    mode++;
+  // static char time = 0;
+  
+  
+  
+  /*Button functionality*/
+  if(switch_state == 0){ /*First switch(Turns green LED on)*/
+    enable_green();
   }
-  if(mode > 3){ /*Reset mode to beginning when reaching the last mode*/
-    mode = 0;
-  }
-  else{
-    if (++blink_count == 1) { /*Executes the brightness every 1 interrupt with current mode*/
-      red_brightness(mode);
-      time++;
-      blink_count = 0;
+  else if (switch_state == 1){ /*Second switch (Flashes LEDs)*/
+
+    if(time == 62){ /*Iterates mode every third of a second*/
+      time = 0;
+      mode++;
     }
+    else if(mode > 3){ /*Reset mode to beginning when reaching the last mode*/
+      mode = 0;
+    }
+    else{ /*Executes methods every 1 interrupt with current mode*/
+      if(++blink_count == 1){
+	red_brightness(mode);
+	green_brightness(mode);
+	time++;
+	blink_count = 0;
+      }
+    }
+    
+  }
+  else if (switch_state == 2){ /*Third switch (Plays jingle and flashes LEDs)*/
+    
+    if(time == 42){ /*Iterates mode every sixth of a second*/
+      time = 0;
+      mode++;
+    }
+    else if(mode > 13){ /*Reset mode to beginning when reaching the last mode*/
+      mode = 0;
+    }
+    else{ /*Executes methods every 1 interrupt with current mode*/
+      if(++blink_count == 1){
+	song2(mode);
+	light_alternate2(mode);
+	time++;
+	blink_count = 0;
+      }
+    }
+    
+  }
+  else if (switch_state == 3){ /*Fourth switch (Turns off speaker and LEDs)*/
+    disable_everything(); 
   }
 }
