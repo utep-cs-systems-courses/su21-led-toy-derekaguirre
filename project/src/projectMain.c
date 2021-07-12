d2e0c46f37347c10c53238273ed2dbbb232731a5
derekaguirre <derekaguirre@me.com>
2021-07-12 02:18:06 -0600

    Updated to handle buttons and speaker

--- project/src/projectMain.c
+++ project/src/projectMain.c
@@ -2,11 +2,15 @@
 #include <msp430.h>
 #include "libTimer.h"
 #include "led.h"
+#include "switches.h"
+#include "buzzer.h"
 
 int main(void) {
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
   led_init();
+  switch_init();
+  buzzer_init();
   enableWDTInterrupts();	/* enable periodic interrupt */
-
+  
   or_sr(0x18);		/* CPU off, GIE on */
 }
